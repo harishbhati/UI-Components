b83c5fa3c0297e7d7565915eb5dae3ff388b606a
harishbhati <46443372+harishbhati@users.noreply.github.com>
2019-01-07 15:59:52 +0530

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+/* gulpfile.js */
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+
+// compile scss
+gulp.task('scss', function () {
+  return gulp.src(['src/scss/*.scss','src/scss/**/*.scss'])
+  .pipe(sass())
+  .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task( 'watch', ['scss'], function () {
+    var sassWatcher = gulp.watch(['src/scss/*.scss','src/scss/**/*.scss'], ['sass']);
+});
+gulp.task('default', ['scss', 'watch' ])

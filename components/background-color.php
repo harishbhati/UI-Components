9b722a0d2cba2073ae9bc5639653b851fcdcfc6c
Harish Bhati <hbhati@ex2india.local>
2019-01-30 12:27:27 +0530

    color and bg-color page

---
+++ components/background-color.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html>
+<?php include 'head.php'; ?>
+    <body>
+        <?php include 'header.php'; ?>
+        <section>
+        <?php include 'sidebar.php'; ?>
+            <main class="content">
+                <div class="page-title-block">
+                    <h1 class="page-title">Background Colors</h1>
+                </div>
+                <section class="medium-spacing">
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Red Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing bg-red"></li>
+                                <li class="medium-spacing bg-red-light"></li>
+                                <li class="medium-spacing bg-red-dark"></li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;bg-red&quot;
+class=&quot;bg-red-light&quot;
+class=&quot;bg-red-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Orange Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing bg-orange"></li>
+                                <li class="medium-spacing bg-orange-light"></li>
+                                <li class="medium-spacing bg-orange-dark"></li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;bg-orange&quot;
+class=&quot;bg-orange-light&quot;
+class=&quot;bg-orange-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Yellow Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing bg-yellow"></li>
+                                <li class="medium-spacing bg-yellow-light"></li>
+                                <li class="medium-spacing bg-yellow-dark"></li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;bg-yellow&quot;
+class=&quot;bg-yellow-light&quot;
+class=&quot;bg-yellow-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Olive Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing bg-olive"></li>
+                                <li class="medium-spacing bg-olive-light"></li>
+                                <li class="medium-spacing bg-olive-dark"></li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;bg-olive&quot;
+class=&quot;bg-olive-light&quot;
+class=&quot;bg-olive-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Green Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing bg-green"></li>
+                                <li class="medium-spacing bg-green-light"></li>
+                                <li class="medium-spacing bg-green-dark"></li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;bg-green&quot;
+class=&quot;bg-green-light&quot;
+class=&quot;bg-green-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Teal Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing bg-teal"></li>
+                                <li class="medium-spacing bg-teal-light"></li>
+                                <li class="medium-spacing bg-teal-dark"></li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;bg-teal&quot;
+class=&quot;bg-teal-light&quot;
+class=&quot;bg-teal-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Blue Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing bg-blue"></li>
+                                <li class="medium-spacing bg-blue-light"></li>
+                                <li class="medium-spacing bg-blue-dark"></li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;bg-blue&quot;
+class=&quot;bg-blue-light&quot;
+class=&quot;bg-blue-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Violet Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing bg-violet"></li>
+                                <li class="medium-spacing bg-violet-light"></li>
+                                <li class="medium-spacing bg-violet-dark"></li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;bg-violet&quot;
+class=&quot;bg-violet-light&quot;
+class=&quot;bg-violet-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Purple Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing bg-purple"></li>
+                                <li class="medium-spacing bg-purple-light"></li>
+                                <li class="medium-spacing bg-purple-dark"></li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;bg-purple&quot;
+class=&quot;bg-purple-light&quot;
+class=&quot;bg-purple-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Pink Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing bg-pink"></li>
+                                <li class="medium-spacing bg-pink-light"></li>
+                                <li class="medium-spacing bg-pink-dark"></li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;bg-pink&quot;
+class=&quot;bg-pink-light&quot;
+class=&quot;bg-pink-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Brown Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing bg-brown"></li>
+                                <li class="medium-spacing bg-brown-light"></li>
+                                <li class="medium-spacing bg-brown-dark"></li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;bg-brown&quot;
+class=&quot;bg-brown-light&quot;
+class=&quot;bg-brown-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Grey Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing bg-grey"></li>
+                                <li class="medium-spacing bg-grey-light"></li>
+                                <li class="medium-spacing bg-grey-dark"></li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;bg-grey&quot;
+class=&quot;bg-grey-light&quot;
+class=&quot;bg-grey-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Black Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing bg-black"></li>
+                                <li class="medium-spacing bg-black-light"></li>
+                                <li class="medium-spacing bg-black-dark"></li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;bg-black&quot;
+class=&quot;bg-black-light&quot;
+class=&quot;bg-black-dark&quot;
+                        </pre>
+                    </div>
+
+                     <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>White Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing bg-white"></li>
+                                <li class="medium-spacing bg-white-light"></li>
+                                <li class="medium-spacing bg-white-dark"></li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;bg-white&quot;
+class=&quot;bg-white-light&quot;
+class=&quot;bg-white-dark&quot;
+                        </pre>
+                    </div>
+                </section>
+            </main>
+    </body>
+</html>
---
+++ components/color.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html>
+<?php include 'head.php'; ?>
+    <body>
+        <?php include 'header.php'; ?>
+        <section>
+        <?php include 'sidebar.php'; ?>
+            <main class="content">
+                <div class="page-title-block">
+                    <h1 class="page-title">Text Colors</h1>
+                </div>
+                <section class="medium-spacing">
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Red Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing border border-black text-red">Red</li>
+                                <li class="medium-spacing border border-black text-red-light">Red Light</li>
+                                <li class="medium-spacing border border-black text-red-dark">Red Dark</li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;text-red&quot;
+class=&quot;text-red-light&quot;
+class=&quot;text-red-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Orange Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing border border-black text-orange">Orange</li>
+                                <li class="medium-spacing border border-black text-orange-light">Orange Light</li>
+                                <li class="medium-spacing border border-black text-orange-dark">Orange Dark</li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;text-orange&quot;
+class=&quot;text-orange-light&quot;
+class=&quot;text-orange-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Yellow Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing border border-black text-yellow">Yellow</li>
+                                <li class="medium-spacing border border-black text-yellow-light">Yellow Light</li>
+                                <li class="medium-spacing border border-black text-yellow-dark">Yellow dark</li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;text-yellow&quot;
+class=&quot;text-yellow-light&quot;
+class=&quot;text-yellow-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Olive Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing border border-black text-olive">Olive</li>
+                                <li class="medium-spacing border border-black text-olive-light">Olive Light</li>
+                                <li class="medium-spacing border border-black text-olive-dark">Olive Dark</li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;text-olive&quot;
+class=&quot;text-olive-light&quot;
+class=&quot;text-olive-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Green Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing border border-black text-green">Green</li>
+                                <li class="medium-spacing border border-black text-green-light">Green Light</li>
+                                <li class="medium-spacing border border-black text-green-dark">Green Dark</li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;text-green&quot;
+class=&quot;text-green-light&quot;
+class=&quot;text-green-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Teal Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing border border-black text-teal">Teal</li>
+                                <li class="medium-spacing border border-black text-teal-light">Teal Light</li>
+                                <li class="medium-spacing border border-black text-teal-dark">Teal dark</li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;text-teal&quot;
+class=&quot;text-teal-light&quot;
+class=&quot;text-teal-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Blue Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing border border-black text-blue">Blue</li>
+                                <li class="medium-spacing border border-black text-blue-light">Blue Light</li>
+                                <li class="medium-spacing border border-black text-blue-dark">Blue Dark</li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;text-blue&quot;
+class=&quot;text-blue-light&quot;
+class=&quot;text-blue-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Violet Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing border border-black text-violet">Violet</li>
+                                <li class="medium-spacing border border-black text-violet-light">Violet Light</li>
+                                <li class="medium-spacing border border-black text-violet-dark">Violet Dark</li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;text-violet&quot;
+class=&quot;text-violet-light&quot;
+class=&quot;text-violet-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Purple Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing border border-black text-purple">Purple</li>
+                                <li class="medium-spacing border border-black text-purple-light">Purple Light</li>
+                                <li class="medium-spacing border border-black text-purple-dark">Purple Dark</li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;text-purple&quot;
+class=&quot;text-purple-light&quot;
+class=&quot;text-purple-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Pink Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing border border-black text-pink">Pink</li>
+                                <li class="medium-spacing border border-black text-pink-light">Pink Light</li>
+                                <li class="medium-spacing border border-black text-pink-dark">Pink Dark</li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;text-pink&quot;
+class=&quot;text-pink-light&quot;
+class=&quot;text-pink-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Brown Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing border border-black text-brown">Brown</li>
+                                <li class="medium-spacing border border-black text-brown-light">Brown Light</li>
+                                <li class="medium-spacing border border-black text-brown-dark">Brown Dark</li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;text-brown&quot;
+class=&quot;text-brown-light&quot;
+class=&quot;text-brown-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Grey Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing border border-black text-grey">Grey</li>
+                                <li class="medium-spacing border border-black text-grey-light">Grey Light</li>
+                                <li class="medium-spacing border border-black text-grey-dark">Grey Dark</li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;text-grey&quot;
+class=&quot;text-grey-light&quot;
+class=&quot;text-grey-dark&quot;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Black Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing border border-black text-black">Black</li>
+                                <li class="medium-spacing border border-black text-black-light">Black Light</li>
+                                <li class="medium-spacing border border-black text-black-dark">Black Dark</li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;text-black&quot;
+class=&quot;text-black-light&quot;
+class=&quot;text-black-dark&quot;
+                        </pre>
+                    </div>
+
+                     <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>White Color Lists</h3>
+                            <p>A list of color patterns.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing border border-black text-white bg-black">White</li>
+                                <li class="medium-spacing border border-black text-white-light bg-black">White Light</li>
+                                <li class="medium-spacing border border-black text-white-dark bg-black">White Dark</li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+class=&quot;text-white&quot;
+class=&quot;text-white-light&quot;
+class=&quot;text-white-dark&quot;
+                        </pre>
+                    </div>
+                </section>
+            </main>
+    </body>
+</html>

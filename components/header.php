b9a5b3ac0716963f653ca819b9563c19fba75419
Harish Bhati <hbhati@ex2india.local>
2019-02-05 10:54:29 +0530

    card page border class

--- components/header.php
+++ components/header.php
@@ -1,5 +1,5 @@
 <header class="flex">
-    <h1 class="logo"><a href="hero-index.php">WebUIKit</a></h1>
+    <h1 class="logo"><a href="index.php">WebUIKit</a></h1>
     <div class="right-links">
         <a href="https://github.com/harishbhati/UI-Components" class="button btn-olive basic border-rounded">DOWNLOAD</a>
     </div>

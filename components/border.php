8289e2ce55f61184dd2498143b0f20e280e9e4bd
Harish Bhati <hbhati@ex2india.local>
2019-02-06 16:16:54 +0530

    container page

---
+++ components/border.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+<?php include 'head.php'; ?>
+    <body>
+        <?php include 'header.php'; ?>
+        <section>
+        <?php include 'sidebar.php'; ?>
+            <main class="content">
+                <div class="page-title-block">
+                    <h1 class="page-title">Border</h1>
+                </div>
+                <section class="medium-spacing">
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Border Radius</h3>
+                            <p>A list of class which are used for rounded corner.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="medium-spacing border border-black radius-0">No Rounded</li>
+                                <li class="medium-spacing border border-black radius-1">Rounded 1</li>
+                                <li class="medium-spacing border border-black radius-2">Rounded 2</li>
+                                <li class="medium-spacing border border-black radius-3">Rounded 3</li>
+                                <li class="medium-spacing border border-black radius-4">Rounded 4</li>
+                                <li class="medium-spacing border border-black radius-5">Rounded 5</li>
+                                <li class="medium-spacing border border-black border-rounded">Rounded Small</li>
+                                <li class="medium-spacing border border-black rounded">Round</li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+&lt;ul class=&quot;unstyled-list inline-list&quot;&gt;
+    &lt;li class=&quot;medium-spacing border border-black radius-0&quot;&gt;No Rounded&lt;/li&gt;
+    &lt;li class=&quot;medium-spacing border border-black radius-1&quot;&gt;Rounded 1&lt;/li&gt;
+    &lt;li class=&quot;medium-spacing border border-black radius-2&quot;&gt;Rounded 2&lt;/li&gt;
+    &lt;li class=&quot;medium-spacing border border-black radius-3&quot;&gt;Rounded 3&lt;/li&gt;
+    &lt;li class=&quot;medium-spacing border border-black radius-4&quot;&gt;Rounded 4&lt;/li&gt;
+    &lt;li class=&quot;medium-spacing border border-black radius-5&quot;&gt;Rounded 5&lt;/li&gt;
+    &lt;li class=&quot;medium-spacing border border-black border-rounded&quot;&gt;Rounded Small&lt;/li&gt;
+    &lt;li class=&quot;medium-spacing border border-black rounded&quot;&gt;Round&lt;/li&gt;
+&lt;/ul&gt;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Border Color</h3>
+                            <p>A list of class which are used for rounded corner.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="m-tiny medium-spacing border border-red">Red Border</li>
+                                <li class="m-tiny medium-spacing border border-orange">Orange Border</li>
+                                <li class="m-tiny medium-spacing border border-yellow">Yellow Border</li>
+                                <li class="m-tiny medium-spacing border border-olive">Olive Border</li>
+                                <li class="m-tiny medium-spacing border border-green">Green Border</li>
+                                <li class="m-tiny medium-spacing border border-teal">Teal Border</li>
+                                <li class="m-tiny medium-spacing border border-blue">Blue Border</li>
+                                <li class="m-tiny medium-spacing border border-violet">Violet Border</li>
+                                <li class="m-tiny medium-spacing border border-purple">Purple Border</li>
+                                <li class="m-tiny medium-spacing border border-pink">Pink Border</li>
+                                <li class="m-tiny medium-spacing border border-brown">Brown Border</li>
+                                <li class="m-tiny medium-spacing border border-grey">Grey Border</li>
+                                <li class="m-tiny medium-spacing border border-black">black Border</li>
+                                <li class="m-tiny medium-spacing border border-white">White Border</li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+&lt;ul class=&quot;unstyled-list inline-list&quot;&gt;
+    &lt;li class=&quot;m-tiny medium-spacing border border-red&quot;&gt;Red Border&lt;/li&gt;
+    &lt;li class=&quot;m-tiny medium-spacing border border-orange&quot;&gt;Orange Border&lt;/li&gt;
+    &lt;li class=&quot;m-tiny medium-spacing border border-yellow&quot;&gt;Yellow Border&lt;/li&gt;
+    &lt;li class=&quot;m-tiny medium-spacing border border-olive&quot;&gt;Olive Border&lt;/li&gt;
+    &lt;li class=&quot;m-tiny medium-spacing border border-green&quot;&gt;Green Border&lt;/li&gt;
+    &lt;li class=&quot;m-tiny medium-spacing border border-teal&quot;&gt;Teal Border&lt;/li&gt;
+    &lt;li class=&quot;m-tiny medium-spacing border border-blue&quot;&gt;Blue Border&lt;/li&gt;
+    &lt;li class=&quot;m-tiny medium-spacing border border-violet&quot;&gt;Violet Border&lt;/li&gt;
+    &lt;li class=&quot;m-tiny medium-spacing border border-purple&quot;&gt;Purple Border&lt;/li&gt;
+    &lt;li class=&quot;m-tiny medium-spacing border border-pink&quot;&gt;Pink Border&lt;/li&gt;
+    &lt;li class=&quot;m-tiny medium-spacing border border-brown&quot;&gt;Brown Border&lt;/li&gt;
+    &lt;li class=&quot;m-tiny medium-spacing border border-grey&quot;&gt;Grey Border&lt;/li&gt;
+    &lt;li class=&quot;m-tiny medium-spacing border border-black&quot;&gt;black Border&lt;/li&gt;
+    &lt;li class=&quot;m-tiny medium-spacing border border-white&quot;&gt;White Border&lt;/li&gt;
+&lt;/ul&gt;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Border Width</h3>
+                            <p>A list of class which are used for boreder width.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="m-tiny medium-spacing border border-black border-1">1 Border</li>
+                                <li class="m-tiny medium-spacing border border-black border-2">2 Border</li>
+                                <li class="m-tiny medium-spacing border border-black border-3">3 Border</li>
+                                <li class="m-tiny medium-spacing border border-black border-4">4 Border</li>
+                                <li class="m-tiny medium-spacing border border-black border-5">5 Border</li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+&lt;ul class=&quot;unstyled-list inline-list&quot;&gt;
+    &lt;li class=&quot;m-tiny medium-spacing border border-black border-1&quot;&gt;1 Border&lt;/li&gt;
+    &lt;li class=&quot;m-tiny medium-spacing border border-black border-2&quot;&gt;2 Border&lt;/li&gt;
+    &lt;li class=&quot;m-tiny medium-spacing border border-black border-3&quot;&gt;3 Border&lt;/li&gt;
+    &lt;li class=&quot;m-tiny medium-spacing border border-black border-4&quot;&gt;4 Border&lt;/li&gt;
+    &lt;li class=&quot;m-tiny medium-spacing border border-black border-5&quot;&gt;5 Border&lt;/li&gt;
+&lt;/ul&gt;
+                        </pre>
+                    </div>
+
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Border Style</h3>
+                            <p>A list of class which are used for boreder style.</p>
+                            <p><strong>Output:</strong></p>
+                            <ul class="unstyled-list inline-list">
+                                <li class="m-tiny medium-spacing border border-black border-solid">Solid Border</li>
+                                <li class="m-tiny medium-spacing border border-black border-dashed">Dashed Border</li>
+                                <li class="m-tiny medium-spacing border border-black border-dotted">Dotted Border</li>
+                            </ul>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+&lt;ul class=&quot;unstyled-list inline-list&quot;&gt;
+    &lt;li class=&quot;m-tiny medium-spacing border border-black border-solid&quot;&gt;Solid Border&lt;/li&gt;
+    &lt;li class=&quot;m-tiny medium-spacing border border-black border-dashed&quot;&gt;Dashed Border&lt;/li&gt;
+    &lt;li class=&quot;m-tiny medium-spacing border border-black border-dotted&quot;&gt;Dotted Border&lt;/li&gt;
+&lt;/ul&gt;
+                        </pre>
+                    </div>
+                </section>
+            </main>
+    </body>
+</html>
---
+++ components/container.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<?php include 'head.php'; ?>
+    <body>
+        <?php include 'header.php'; ?>
+        <section>
+        <?php include 'sidebar.php'; ?>
+            <main class="content">
+                <div class="page-title-block">
+                    <h1 class="page-title">Containers</h1>
+                </div>
+                <section class="medium-spacing">
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <p>A container is an element designed to contain page elements to a reasonable maximum width based on the size of a user's screen.
+                            This size is calculated programmatically by determining the maximum device width available within a device breakpoint, given a desired minimum gutter size.
+                            We are basically used two type of containers Fixed width and Full width.
+                            </p>
+                        </div>
+                        <div class="outputs">
+                            <h3>Fixed Width Container</h3>
+                            <p>A class 'container-fixed' are used for applying fixed width container.</p>
+                            <p><strong>Output:</strong></p>
+                            <div class="container-fixed">
+                                <ul class="unstyled-list inline-list">
+                                    <li class="m-tiny medium-spacing bg-grey">Fixed Container</li>
+                                    <li class="m-tiny medium-spacing bg-grey">Fixed Container</li>
+                                    <li class="m-tiny medium-spacing bg-grey">Fixed Container</li>
+                                    <li class="m-tiny medium-spacing bg-grey">Fixed Container</li>
+                                    <li class="m-tiny medium-spacing bg-grey">Fixed Container</li>
+                                    <li class="m-tiny medium-spacing bg-grey">Fixed Container</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+&lt;div class=&quot;container-fixed&quot;&gt;
+    &lt;ul class=&quot;unstyled-list inline-list&quot;&gt;
+        &lt;li class=&quot;m-tiny medium-spacing bg-grey&quot;&gt;Fixed Container&lt;/li&gt;
+        &lt;li class=&quot;m-tiny medium-spacing bg-grey&quot;&gt;Fixed Container&lt;/li&gt;
+        &lt;li class=&quot;m-tiny medium-spacing bg-grey&quot;&gt;Fixed Container&lt;/li&gt;
+        &lt;li class=&quot;m-tiny medium-spacing bg-grey&quot;&gt;Fixed Container&lt;/li&gt;
+        &lt;li class=&quot;m-tiny medium-spacing bg-grey&quot;&gt;Fixed Container&lt;/li&gt;
+        &lt;li class=&quot;m-tiny medium-spacing bg-grey&quot;&gt;Fixed Container&lt;/li&gt;
+    &lt;/ul&gt;
+&lt;/div&gt;
+                        </pre>
+                    </div>
+
+                    <div class="outputs">
+                            <h3>Full Width Container</h3>
+                            <p>A class 'container-fluid' are used for applying full width container.</p>
+                            <p><strong>Output:</strong></p>
+                            <div class="container-fluid">
+                                <ul class="unstyled-list inline-list">
+                                    <li class="m-tiny medium-spacing bg-grey">Full Container</li>
+                                    <li class="m-tiny medium-spacing bg-grey">Full Container</li>
+                                    <li class="m-tiny medium-spacing bg-grey">Full Container</li>
+                                    <li class="m-tiny medium-spacing bg-grey">Full Container</li>
+                                    <li class="m-tiny medium-spacing bg-grey">Full Container</li>
+                                    <li class="m-tiny medium-spacing bg-grey">Full Container</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+&lt;div class=&quot;container-fluid&quot;&gt;
+    &lt;ul class=&quot;unstyled-list inline-list&quot;&gt;
+        &lt;li class=&quot;m-tiny medium-spacing bg-grey&quot;&gt;Full Container&lt;/li&gt;
+        &lt;li class=&quot;m-tiny medium-spacing bg-grey&quot;&gt;Full Container&lt;/li&gt;
+        &lt;li class=&quot;m-tiny medium-spacing bg-grey&quot;&gt;Full Container&lt;/li&gt;
+        &lt;li class=&quot;m-tiny medium-spacing bg-grey&quot;&gt;Full Container&lt;/li&gt;
+        &lt;li class=&quot;m-tiny medium-spacing bg-grey&quot;&gt;Full Container&lt;/li&gt;
+        &lt;li class=&quot;m-tiny medium-spacing bg-grey&quot;&gt;Full Container&lt;/li&gt;
+    &lt;/ul&gt;
+&lt;/div&gt;
+                        </pre>
+                    </div>
+                    </div>
+                </section>
+            </main>
+    </body>
+</html>

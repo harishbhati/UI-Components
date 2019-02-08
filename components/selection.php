e7314ff117c43162a4943b04d04d7e50c37e8e3b
Harish Bhati <hbhati@ex2india.local>
2019-02-08 12:16:13 +0530

    selection page and scss

---
+++ components/selection.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html>
+<?php include 'head.php'; ?>
+    <body>
+        <?php include 'header.php'; ?>
+        <section>
+        <?php include 'sidebar.php'; ?>
+            <main class="content">
+                <div class="page-title-block">
+                    <h1 class="page-title">Selection Background Color</h1>
+                </div>
+                <section class="medium-spacing">                
+                    <p>You can change the background color and color of selected text by styling.Styling this pseudo element is great for matching user-selected text to your sites color scheme</p>
+                    <div class="large-spacing">
+                        <div class="selection-red">
+                            <h3>Red Selection</h3>
+                            <p>A class which are used selection bg-color red.</p>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+&lt;div class=&quot;selection-red&quot;&gt;
+    &lt;h3&gt;Red Selection&lt;/h3&gt;
+    &lt;p&gt;A class which are used selection bg-color red.&lt;/p&gt;
+&lt;/div&gt;
+                        </pre>
+                        <div class="selection-orange">
+                            <h3>Orange Selection</h3>
+                            <p>A class which are used selection bg-color red.</p>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+&lt;div class=&quot;selection-orange&quot;&gt;
+    &lt;h3&gt;Orange Selection&lt;/h3&gt;
+    &lt;p&gt;A class which are used selection bg-color orange.&lt;/p&gt;
+&lt;/div&gt;
+                        </pre>
+                        <div class="selection-yellow">
+                            <h3>Yellow Selection</h3>
+                            <p>A class which are used selection bg-color yellow.</p>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+&lt;div class=&quot;selection-yellow&quot;&gt;
+    &lt;h3&gt;Yellow Selection&lt;/h3&gt;
+    &lt;p&gt;A class which are used selection bg-color yellow.&lt;/p&gt;
+&lt;/div&gt;
+                        </pre>
+                        <div class="selection-olive">
+                            <h3>Olive Selection</h3>
+                            <p>A class which are used selection bg-color olive.</p>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+&lt;div class=&quot;selection-olive&quot;&gt;
+    &lt;h3&gt;Olive Selection&lt;/h3&gt;
+    &lt;p&gt;A class which are used selection bg-color olive.&lt;/p&gt;
+&lt;/div&gt;
+                        </pre>
+                        <div class="selection-green">
+                            <h3>Green Selection</h3>
+                            <p>A class which are used selection bg-color green.</p>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+&lt;div class=&quot;selection-green&quot;&gt;
+    &lt;h3&gt;Green Selection&lt;/h3&gt;
+    &lt;p&gt;A class which are used selection bg-color green.&lt;/p&gt;
+&lt;/div&gt;
+                        </pre>
+                        <div class="selection-teal">
+                            <h3>Teal Selection</h3>
+                            <p>A class which are used selection bg-color teal.</p>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+&lt;div class=&quot;selection-teal&quot;&gt;
+    &lt;h3&gt;Teal Selection&lt;/h3&gt;
+    &lt;p&gt;A class which are used selection bg-color teal.&lt;/p&gt;
+&lt;/div&gt;
+                        </pre>
+                        <div class="selection-blue">
+                            <h3>Blue Selection</h3>
+                            <p>A class which are used selection bg-color beal.</p>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+&lt;div class=&quot;selection-blue&quot;&gt;
+    &lt;h3&gt;Blue Selection&lt;/h3&gt;
+    &lt;p&gt;A class which are used selection bg-color blue.&lt;/p&gt;
+&lt;/div&gt;
+                        </pre>
+                        <div class="selection-violet">
+                            <h3>Violet Selection</h3>
+                            <p>A class which are used selection bg-color violet.</p>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+&lt;div class=&quot;selection-violet&quot;&gt;
+    &lt;h3&gt;Violet Selection&lt;/h3&gt;
+    &lt;p&gt;A class which are used selection bg-color violet.&lt;/p&gt;
+&lt;/div&gt;
+                        </pre>
+                        <div class="selection-purple">
+                            <h3>Purple Selection</h3>
+                            <p>A class which are used selection bg-color purple.</p>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+&lt;div class=&quot;selection-purple&quot;&gt;
+    &lt;h3&gt;Purple Selection&lt;/h3&gt;
+    &lt;p&gt;A class which are used selection bg-color purple.&lt;/p&gt;
+&lt;/div&gt;
+                        </pre>
+                        <div class="selection-pink">
+                            <h3>Pink Selection</h3>
+                            <p>A class which are used selection bg-color pink.</p>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+&lt;div class=&quot;selection-pink&quot;&gt;
+    &lt;h3&gt;Pink Selection&lt;/h3&gt;
+    &lt;p&gt;A class which are used selection bg-color pink.&lt;/p&gt;
+&lt;/div&gt;
+                        </pre>
+                        <div class="selection-brown">
+                            <h3>Brown Selection</h3>
+                            <p>A class which are used selection bg-color brown.</p>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+&lt;div class=&quot;selection-brown&quot;&gt;
+    &lt;h3&gt;Brown Selection&lt;/h3&gt;
+    &lt;p&gt;A class which are used selection bg-color brown.&lt;/p&gt;
+&lt;/div&gt;
+                        </pre>
+                        <div class="selection-black">
+                            <h3>Black Selection</h3>
+                            <p>A class which are used selection bg-color black.</p>
+                        </div>
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+&lt;div class=&quot;selection-black&quot;&gt;
+    &lt;h3&gt;Black Selection&lt;/h3&gt;
+    &lt;p&gt;A class which are used selection bg-color black.&lt;/p&gt;
+&lt;/div&gt;
+                        </pre>
+                    </div>
+                </section>
+            </main>
+    </body>
+</html>

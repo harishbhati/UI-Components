ffae4479b229a7b17f411d354bcde8f0e1e89873
Harish Bhati <hbhati@ex2india.local>
2019-02-15 17:09:18 +0530

    installation-page

--- components/introduction.php
+++ components/introduction.php
@@ -11,11 +11,11 @@
             </div>
                 <section class="large-spacing">
                     <h3>Overview</h3>
-                    <p><strong>A complete solution:</strong> Web UIKit is a complete CSS framework that is production ready out of the box.  There is no need to customize the CSS if you don’t want to because it is head and shoulders
+                    <p><strong>A complete solution:</strong> WebUIKit is a complete CSS framework that is production ready out of the box.  There is no need to customize the CSS if you don’t want to because it is head and shoulders
                      above others in terms of the way that it looks.  We think it looks great and hope you do too.</p>
                      <p><strong>Minimal CSS:</strong> This entire site was created with an attempt to only use the CSS provided by Web UIKit.  The additions made via custom css were minimal.  That’s an impressive feat considering that 
                      the entire framework comes in at <strong>8kb</strong> when gzipped.</p>
-                     <p><strong>Community driven:</strong> Web UIKit is created as a free open source framework. You are invited to contribute to the source 
+                     <p><strong>Community driven:</strong> WebUIKit is created as a free open source framework. You are invited to contribute to the source 
                      by getting involved on <a href="https://github.com/harishbhati/UI-Components">GitHub</a> .</p>
                      <p>Alternatively, you can use either cdnjs to import mini.css into your HTML page's <head> tag:</p>
                      <pre class="markup">&lt;link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/harishbhati/WebUIkit@1.0/dist/css/main.css"&gt;</pre>
@@ -24,23 +24,24 @@
                 </section>
                 <section class="large-spacing">
                     <h3>Structure</h3>
-                    <p>Web UIKit includes a directory structure that separates the CSS framework into modules which are included from a global file. 
+                    <p>WebUIKit includes a directory structure that separates the CSS framework into modules which are included from a global file. 
                         This allows you to pick and choose what you'd like to include into your project or just pull in everything.</p>
                     <pre class="markup">
-                        <code class="code">mustard-ui/
+                        <code class="code">WebUIKit/
+                            ├── components/
                             ├── dist/
                             |   └── css/
-                            |       └── web-uikit.min.css
+                            |       └── webuikit.min.css
                             |   └── js/
-                            |       └── *.js
+                            |       └── webuikit.js
                             └── src/
                                 └── scss/
-                                    ├── components/
+                                    |
                                     ├── elements/
                                     ├── layout/
                                     ├── mixins/
                                     ├── vars/
-                                    └── web-uikit.scss
+                                    └── webuikit.scss
                         </code>
                     </pre>
                 </section>

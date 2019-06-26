a525bfa3a7fe97c2de245f31b660eb5bbd777956
Harish Bhati <hbhati@ex2india.local>
2019-06-26 14:53:43 +0530

    image banner block

---
+++ components/banner.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+<?php include 'head.php'; ?>
+    <body class="inner-scroll">
+        <?php include 'header.php'; ?>
+        <section>
+        <section class="sidebar-content-holder sidebar-left">
+        <?php include 'sidebar.php'; ?>
+            <main class="content nicescroll-block">
+                <div class="page-title-block">
+                    <h1 class="page-title">Banner</h1>
+                </div>
+                <section class="medium-spacing">
+                    <div class="large-spacing">
+                    <p><strong>List Of Overlay color Classes:</strong></p>
+                        <pre class="markup">
+'overlay-red'
+'overlay-orange'
+'overlay-yellow'
+'overlay-olive'
+'overlay-green'
+'overlay-teal'
+'overlay-blue'
+'overlay-violet'
+'overlay-purple'
+'overlay-pink'
+'overlay-brown'
+'overlay-grey'
+'overlay-black'
+'overlay-white'
+                        </pre>
+                        <div class="outputs">
+                            <h3>Banner Image With Left Caption</h3>
+                            <p>The example below shows image With left caption</p>
+                            <p><strong>Output:</strong></p>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <div class="banner-holder overlay-white caption-left">
+                        <img src="http://www.shreejiholidays.com/images/slider/img5.jpg" alt="Banner for Home" class="img-responsive img-center">
+                        <div class="caption">
+                            <h2>Caption Title</h2>
+                            <p>It is simply dummy text of the printing and typesetting industry. It is simply dummy text of the printing and typesetting industry</p>
+                            <a class="button basic black inverted" href="#" title="Learn More">Learn More</a>
+                        </div>
+                    </div>
+                    <div class="large-spacing">
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+&lt;div class=&quot;banner-holder overlay-white caption-left&quot;&gt;
+    &lt;img src=&quot;http://www.shreejiholidays.com/images/slider/img5.jpg&quot; alt=&quot;Banner for Home&quot; class=&quot;img-responsive img-center&quot;&gt;
+&lt;div class=&quot;caption&quot;&gt;
+    &lt;h2&gt;Caption Title&lt;/h2&gt;
+    &lt;p&gt;It is simply dummy text of the printing and typesetting industry. It is simply dummy text of the printing and typesetting industry&lt;/p&gt;
+    &lt;a class=&quot;button basic black inverted&quot; href=&quot;#&quot; title=&quot;Learn More&quot;&gt;Learn More&lt;/a&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+                        </pre>
+                    </div>
+                </section>
+                <section class="medium-spacing">
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Banner Image With Center Caption</h3>
+                            <p>The example below shows image With center caption</p>
+                            <p><strong>Output:</strong></p>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <div class="banner-holder overlay-yellow caption-center">
+                        <img src="http://www.shreejiholidays.com/images/slider/img5.jpg" alt="Banner for Home" class="img-responsive img-center">
+                        <div class="caption text-center">
+                            <h2>Caption Title</h2>
+                            <p>It is simply dummy text of the printing and typesetting industry. It is simply dummy text of the printing and typesetting industry</p>
+                            <a class="button btn-teal" href="#" title="Learn More">Learn More</a>
+                        </div>
+                    </div>
+                    <div class="large-spacing">
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+&lt;div class=&quot;banner-holder overlay-yellow caption-center&quot;&gt;
+    &lt;img src=&quot;http://www.shreejiholidays.com/images/slider/img5.jpg&quot; alt=&quot;Banner for Home&quot; class=&quot;img-responsive img-center&quot;&gt;
+&lt;div class=&quot;caption text-center&quot;&gt;
+    &lt;h2&gt;Caption Title&lt;/h2&gt;
+    &lt;p&gt;It is simply dummy text of the printing and typesetting industry. It is simply dummy text of the printing and typesetting industry&lt;/p&gt;
+    &lt;a class=&quot;button btn-teal&quot; href=&quot;#&quot; title=&quot;Learn More&quot;&gt;Learn More&lt;/a&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+                        </pre>
+                    </div>
+                </section>
+                <section class="medium-spacing">
+                    <div class="large-spacing">
+                        <div class="outputs">
+                            <h3>Banner Image With Right Caption</h3>
+                            <p>The example below shows image With right caption</p>
+                            <p><strong>Output:</strong></p>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <div class="banner-holder overlay-teal caption-right">
+                        <img src="http://www.shreejiholidays.com/images/slider/img5.jpg" alt="Banner for Home" class="img-responsive img-center">
+                        <div class="caption">
+                            <h2>Caption Title</h2>
+                            <p>It is simply dummy text of the printing and typesetting industry. It is simply dummy text of the printing and typesetting industry</p>
+                            <a class="button btn-olive" href="#" title="Learn More">Learn More</a>
+                        </div>
+                    </div>
+                    <div class="large-spacing">
+                        <p><strong>Code Example:</strong></p>
+                        <pre class="markup">
+&lt;div class=&quot;banner-holder overlay-white caption-right&quot;&gt;
+    &lt;img src=&quot;http://www.shreejiholidays.com/images/slider/img5.jpg&quot; alt=&quot;Banner for Home&quot; class=&quot;img-responsive img-center&quot;&gt;
+&lt;div class=&quot;caption&quot;&gt;
+    &lt;h2&gt;Caption Title&lt;/h2&gt;
+    &lt;p&gt;It is simply dummy text of the printing and typesetting industry. It is simply dummy text of the printing and typesetting industry&lt;/p&gt;
+    &lt;a class=&quot;button btn-olive&quot; href=&quot;#&quot; title=&quot;Learn More&quot;&gt;Learn More&lt;/a&gt;
+&lt;/div&gt;
+&lt;/div&gt;
+                        </pre>
+                    </div>
+                </section>
+            </section>
+        </main>
+        <?php include 'footer.php'; ?>
+        <?php include 'script.php'; ?>
+    </body>
+</html>
